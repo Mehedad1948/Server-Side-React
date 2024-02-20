12e72e4fffd56f2f14e8196086441017e3f41a9a
Mehrdad Nouri <mnourib12@gmail.com>
2024-02-20 10:11:24 +0330

    Server bundle optimization

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
     //  Inform webpack that we are building a bundle for nodejs,
@@ -13,8 +14,10 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
     // Tell webpack to run babel on every file it runs through
+
+    externals: [webpackNodeExternals()]
 
 }
 
