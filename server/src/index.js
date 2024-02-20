0a3ddab7de7c207593e26b05df1d8423c6d0deb0
Mehrdad Nouri <mnourib12@gmail.com>
2024-02-21 00:58:59 +0330

    initiate server redux

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 // Es2015 , To be similar to client codes , make possible by webpack
 import express from 'express'
 import renderer from './helpers/renderer'
+import createaStore from './helpers/createStore'
 
 const app = express()
 
@@ -8,8 +9,9 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('*', (req, res) => {
- 
-    res.send(renderer(req))
+    const store = createStore()
+
+    res.send(renderer(req, store))
 })
 
 app.listen(3000, () => {
